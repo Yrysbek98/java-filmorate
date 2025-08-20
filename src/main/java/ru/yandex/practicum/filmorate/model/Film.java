a68c8a96aceb42379af27d3a0040381bcf9d8ae7
Yrysbek Abduvaliev <abduvalievyrysbek98@gmail.com>
2025-08-20 14:27:55 +0600

    Добавил основные классы

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,12 +1,21 @@
 package ru.yandex.practicum.filmorate.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.time.LocalDate;
 
 /**
  * Film.
  */
-@Getter
-@Setter
+@Data
+@AllArgsConstructor
 public class Film {
+    private int id;
+    private String name;
+    private String description;
+    private LocalDate releaseDate;
+    private long duration;
 }
