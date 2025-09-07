459104c98e1cd4624f1adeb75c1a8e3c7387bd63
Yrysbek Abduvaliev <abduvalievyrysbek98@gmail.com>
2025-09-07 13:24:26 +0600

    исправил ошибку в CodeStyle1 в AbstractDtoException

--- src/main/java/ru/yandex/practicum/filmorate/exception/AbstractDtoException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/AbstractDtoException.java
@@ -4,14 +4,16 @@ import org.springframework.http.HttpStatus;
 import ru.yandex.practicum.filmorate.dto.ErrorResponse;
 
 
-public abstract  class AbstractDtoException extends RuntimeException {
+public abstract class AbstractDtoException extends RuntimeException {
     private HttpStatus httpStatus;
+
     public AbstractDtoException(String message, HttpStatus httpStatus) {
         super(message);
         this.httpStatus = httpStatus;
     }
-    public ErrorResponse toResponse(){
-       return  new ErrorResponse(getMessage(), httpStatus);
+
+    public ErrorResponse toResponse() {
+        return new ErrorResponse(getMessage(), httpStatus);
 
     }
 }
