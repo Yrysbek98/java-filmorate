7f91a43a06604a4dde6d6c4811a58465af922d52
Yrysbek Abduvaliev <abduvalievyrysbek98@gmail.com>
2025-09-29 22:21:41 +0600

    тесты

--- src/test/java/ru/yandex/practicum/filmorate/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmControllerTest.java
@@ -1,53 +1,15 @@
 package ru.yandex.practicum.filmorate;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.test.context.SpringBootTest;
-import ru.yandex.practicum.filmorate.controller.FilmController;
-import ru.yandex.practicum.filmorate.model.Film;
-import ru.yandex.practicum.filmorate.storage.InMemoryFilmStorage;
 
 
-import java.time.LocalDate;
-import java.util.Collection;
+
 
 @SpringBootTest
 public class FilmControllerTest {
 
-    @Autowired
-    private FilmController filmController;
 
-    @Autowired
-    private InMemoryFilmStorage filmStorage;
-
-    @BeforeEach
-    void cleanUp() {
-        filmStorage.clear();
-    }
-
-    @Test
-    void getFilms() {
-        Film film = new Film("Титаник", "В первом и последнем плавании шикарного «Титаника» встречаются двое.", LocalDate.of(1997, 12, 19), 194);
-        Film film1 = new Film("Бойцовский клуб", "Терзаемый хронической бессонницей и отчаянно пытающийся вырваться из мучительно скучной жизни клерк встречает некоего Тайлера Дардена, харизматического торговца мылом с извращенной философией.", LocalDate.of(1999, 10, 15), 149);
-
-        filmController.createFilm(film);
-        filmController.createFilm(film1);
-
-        Collection<Film> films = filmController.findAllFilms();
-        System.out.println(films);
-        Assertions.assertEquals(2, films.size(), "Неправильно выполнен метод получения фильмов");
-    }
-
-    @Test
-    void createFilm() {
-        Film film = new Film("Древний фильм", "Фильм до рождение кино", LocalDate.of(2000, 12, 12), 60);
-        filmController.createFilm(film);
-
-        Collection<Film> films = filmController.findAllFilms();
-        Assertions.assertEquals(1, films.size(), "Неправильно выполнено добавление фильма");
-    }
 
 
 }
--- src/test/java/ru/yandex/practicum/filmorate/UserControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/UserControllerTest.java
@@ -1,55 +1,10 @@
 package ru.yandex.practicum.filmorate;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import ru.yandex.practicum.filmorate.controller.UserController;
-import ru.yandex.practicum.filmorate.model.User;
-import ru.yandex.practicum.filmorate.storage.InMemoryUserStorage;
 
-import java.time.LocalDate;
-import java.util.Collection;
 
 @SpringBootTest
 public class UserControllerTest {
 
-    @Autowired
-    private UserController userController;
-
-    @Autowired
-    private InMemoryUserStorage userStorage;
-
-    @BeforeEach
-    void cleanUp() {
-        userStorage.clear();
-    }
-
-    @Test
-    void getUsers() {
-        User user1 = new User("resbek@gmail.com", "resbek", "Yrysbek", LocalDate.of(2000, 12, 5));
-        User user2 = new User("resbek98@gmail.com", "Yrysbek", "resbek", LocalDate.of(1998, 12, 5));
-
-        userController.createUser(user1);
-        userController.createUser(user2);
-
-        Collection<User> users = userController.findAll();
-        Assertions.assertEquals(2, users.size(), "Неправильно выполнен метод получения пользователей");
-    }
-
-    @Test
-    void createUser() {
-        User user1 = new User("resbek@gmail.com", "resbek", "Yrysbek", LocalDate.of(2000, 12, 5));
-
-        userController.createUser(user1);
-
-        Collection<User> users = userController.findAll();
-        Assertions.assertEquals(1, users.size(), "Неправильно выполнено добавление пользователя");
-
-
-        User storedUser = users.iterator().next();
-        Assertions.assertEquals("Yrysbek", storedUser.getName(), "Имя пользователя не установлено корректно");
-    }
 
 }
