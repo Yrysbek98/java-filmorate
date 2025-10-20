f235a84a544b463105fd84718721eadb775a2407
Yrysbek Abduvaliev <abduvalievyrysbek98@gmail.com>
2025-10-21 02:14:27 +0600

    устанавливаем изначальное значение в поле useful

--- src/main/java/ru/yandex/practicum/filmorate/model/Review.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Review.java
@@ -11,7 +11,7 @@ public class Review {
     private Integer reviewId;
     private String content;
     private Boolean isPositive;
-    private int useful;
+    private int useful = 0;
     private Integer userId;
     private Integer filmId;
 }
