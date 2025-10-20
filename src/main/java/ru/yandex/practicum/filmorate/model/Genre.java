4324ad0764f17ba51e9d411255a1c732c4aa3c6c
Yrysbek Abduvaliev <abduvalievyrysbek98@gmail.com>
2025-10-21 03:06:48 +0600

    небольшое изменение в Genre.java

--- src/main/java/ru/yandex/practicum/filmorate/model/Genre.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Genre.java
@@ -14,20 +14,4 @@ public class Genre {
     private int id;
     private String name;
 
-    public Genre(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Genre)) return false;
-        Genre genre = (Genre) o;
-        return id == genre.id;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
 }
