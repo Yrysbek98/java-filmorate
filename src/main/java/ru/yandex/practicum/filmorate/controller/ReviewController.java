e574e73ee2cc178be3a85d4f3e0da422418a138b
Yrysbek Abduvaliev <abduvalievyrysbek98@gmail.com>
2025-10-22 01:00:08 +0600

    немного изменил контроллер createReview()

--- src/main/java/ru/yandex/practicum/filmorate/controller/ReviewController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ReviewController.java
@@ -37,7 +37,7 @@ public class ReviewController {
 
     @PostMapping
     public Review createReview(@Valid @RequestBody  Review review) {
-
+        review.setUseful(0);
         return reviewServiceDB.createReview(review);
     }
 
